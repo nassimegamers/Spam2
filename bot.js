08faa1f1c26156ab2304eba5b4d1ce01d9785dbd
nassimegamers <44312622+nassimegamers@users.noreply.github.com>
2019-01-18 11:48:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,13 +23,13 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+        message.channel.send('#credits @.NaSsimééé#0001 ')
     }
 });
 
 client.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#credits @.NaSsimééé#0001 ')
     }
 });
 
@@ -41,13 +41,13 @@ client2.on('message', message => {
 
 client2.on('message', message => {
     if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+        message.channel.send('#credits @.NaSsimééé#0001 ')
     }
 });
 
 client2.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#credits @.NaSsimééé#0001 ')
     }
 });
 
